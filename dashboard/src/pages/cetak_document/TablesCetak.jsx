78258c35e4589b7eadcc7bd3021ab9af52048001
masfajaranugrah <massfajar223@gamil.com>
2024-06-01 22:56:28 +0700

    update excel new

--- dashboard/src/pages/cetak_document/TablesCetak.jsx
+++ dashboard/src/pages/cetak_document/TablesCetak.jsx
@@ -1,35 +1,48 @@
-import React, { useCallback, useMemo, useRef, useState, useEffect, StrictMode } from "react";
-import axios from 'axios';
-import { AgGridReact } from "@ag-grid-community/react";
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+  useEffect,
+} from "react";
+ import { AgGridReact } from "@ag-grid-community/react";
 import "@ag-grid-community/styles/ag-grid.css";
 import "@ag-grid-community/styles/ag-theme-quartz.css";
+ import axios from 'axios'
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { ExcelExportModule } from "@ag-grid-enterprise/excel-export";
+import { MenuModule } from "@ag-grid-enterprise/menu";
+import { SetFilterModule } from "@ag-grid-enterprise/set-filter";
 import { ModuleRegistry } from "@ag-grid-community/core";
 ModuleRegistry.registerModules([
   ClientSideRowModelModule,
-  ExcelExportModule
+  ExcelExportModule,
+  MenuModule,
+  SetFilterModule,
 ]);
-
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 const TablesCetak = () => {
   const gridRef = useRef();
   const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
   const gridStyle = useMemo(() => ({ height: "500px", width: "100%" }), []);
-  const [rowData, setRowData] = useState([]);
-  const [columnDefs] = useState([
-    { field: "id" },
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "id" , minWidth: 200},
     { field: "nim" },
-    { field: "nama" },
+    { field: "nama", minWidth: 200 },
     { field: "email" },
-    { field: "nomer telp" },
+    { field: "nomer telp", minWidth: 150 },
     { field: "nomer Antrian" },
-    { field: "status" }
+    { field: "status" },
   ]);
-  const defaultColDef = useMemo(() => ({
-    filter: true,
-    minWidth: 100,
-    flex: 1
-  }), []);
+  // const defaultColDef = useMemo(() => {
+  //   return {
+  //     filter: true,
+  //     minWidth: 100,
+  //     flex: 1,
+  //   };
+  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,23 +68,23 @@ const TablesCetak = () => {
 
   const onBtExport = useCallback(() => {
     const names = {};
-    gridRef.current.api.forEachNode(node => {
+    gridRef.current.api.forEachNode(function (node) {
       if (!names[node.data.nama]) {
         names[node.data.nama] = true;
       }
     });
+    let spreadsheets = [];
     const performExport = async () => {
-      let spreadsheets = [];
-      for (const name in names) {
+      for (const nama in names) {
         await gridRef.current.api.setColumnFilterModel("nama", {
-          values: [name]
+          values: [nama],
         });
         gridRef.current.api.onFilterChanged();
-        if (!gridRef.current.api.isAnyFilterPresent()) {
-          throw new Error("Filter not applied");
+        if (gridRef.current.api.getColumnFilterModel("nama") == null) {
+          throw new Error("Example error: Filter not applied");
         }
         const sheet = gridRef.current.api.getSheetDataForExcel({
-          sheetName: name
+          sheetName: nama,
         });
         if (sheet) {
           spreadsheets.push(sheet);
@@ -81,40 +94,63 @@ const TablesCetak = () => {
       gridRef.current.api.onFilterChanged();
       gridRef.current.api.exportMultipleSheetsAsExcel({
         data: spreadsheets,
-        fileName: "data_antrian.xlsx"
+        fileName: "data_antrian.xlsx",
       });
+      spreadsheets = [];
     };
     performExport();
-  }, []);
 
+
+
+   }, []);
+// console.log(spreadsheets)
+
+const defaultColDef = useMemo(() => {
+  return {
+    filter: 'agTextColumnFilter',
+    floatingFilter: true,
+  }
+}, []);
   return (
-    <StrictMode>
-      <div style={containerStyle}>
-        <div className="container">
-          <div>
-            <button
-              onClick={onBtExport}
-              style={{ marginBottom: "5px", fontWeight: "bold" }}
-            >
-              Export to Excel
-            </button>
-          </div>
-          <div className="grid-wrapper">
-            <div
-              style={gridStyle}
-              className={"ag-theme-quartz-dark"}
-            >
-              <AgGridReact
-                ref={gridRef}
-                rowData={rowData}
-                columnDefs={columnDefs}
-                defaultColDef={defaultColDef}
-              />
-            </div>
+    <div style={containerStyle}>
+      <div className="container">
+        <div>
+       
+          <Button
+           onClick={onBtExport}
+           style={{ marginBottom: "40px", fontWeight: "bold" }}
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<CloudUploadIcon />}
+    >
+       Export to Excel
+    </Button>
+        </div>
+        <div className="grid-wrapper">
+          <div
+            style={gridStyle}
+            className={
+              "ag-theme-quartz"
+            }
+          >
+            <AgGridReact
+              ref={gridRef}
+              rowData={rowData}
+              columnDefs={columnDefs}
+              defaultColDef={defaultColDef}
+              onGridReady={rowData}
+              rowSelection="multiple"
+              suppressRowClickSelection={true}
+              pagination={true}
+              paginationPageSize={10}
+              paginationPageSizeSelector={[10, 25, 50]}
+            />
           </div>
         </div>
       </div>
-    </StrictMode>
+    </div>
   );
 };
 
